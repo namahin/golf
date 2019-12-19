0b33215df142783cbf2b0aacb728d9bcefc62299
PyFellow <50703361+pyfellow@users.noreply.github.com>
2019-12-19 22:42:40 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// active wow js
+new WOW().init();
+// owl carousel
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoWidth: true,
+    margin: 30,
+    nav: true,
+    animateIn: 'flipInX',
+    // autoplay: true,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: true
+})
+
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
+
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY > 0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
